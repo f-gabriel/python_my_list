2b766bb0a2cc164882d170dd335fcc27f4f8fa2e
Fredrik <fredn@chalmers.se>
2026-03-23 08:02:06 +0100

    överför

--- my_dictionary.py
+++ my_dictionary.py
@@ -8,23 +8,39 @@ class My_Dictionary(Containers):
 
 
     def append(self, key, value = None):
+        if key in self:
+            self[key] = value
         item = Container(key)
         item.append(value)
         super().append(item)
     
-    def items(self):
+    def items(self): # inte klar
         items = Containers()
         for item in self:
             items.append(item)
         return items
     
+    def get_key(self):
+        return self.value.value
     
+    def get_item_value(self):
+        return self.value.next_value
+    
+    def __repr__(self):
+        representation = ''
+        for item in self:
+            key = str(item.get_key())
+            value = str(item.get_item_value())
+            key_value_string = f'{key}: {value}'
+            representation += key_value_string + ', '    
+        representation = representation[:-2]
+        return '{' + representation + '}'
 
     def __getitem__(self, key):
         item: My_Dictionary
         for item in self:
-            if self.key == key:
-                return item.get_value()   
+            if self.get_key() == key:
+                return item.get_item_value()   
         raise KeyError  
 
     def __contains__(self, item):
@@ -35,5 +51,7 @@ class My_Dictionary(Containers):
         return False
 
     def __setitem__(self, key, value):
-        if key in self:
-            self      
+        item: My_Dictionary
+        for item in self:
+            if self.get_key() == key:
+
